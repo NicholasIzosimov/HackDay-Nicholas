323cd9415e056a162952153b243b55c03e85ea20
NicholasIzosimov <104497770+NicholasIzosimov@users.noreply.github.com>
2022-07-08 15:47:32 +0200

    Add buy functionality (mock)

--- Hackday/ChoreGenerator/Controllers/ChoreGeneratorController.cs
+++ Hackday/ChoreGenerator/Controllers/ChoreGeneratorController.cs
@@ -25,11 +25,11 @@ public class ChoreGeneratorController : ControllerBase
     [HttpGet("chores/{id}")]
     public IActionResult GetChore(int id)
     {
-        var chore = new Chore() {Id = id};
+        var chore = new Chore() { Id = id };
         var oneChore = _context.Chores.Find(id);
         return Ok(oneChore);
     }
-    
+
     [HttpGet("rewards")]
     public IActionResult GetRewards()
     {
@@ -39,37 +39,42 @@ public class ChoreGeneratorController : ControllerBase
     [HttpPost("chores")]
     public async Task<IActionResult> AddChores(ChoreRequest request)
     {
-        var newChore = new Chore() { Task = request.Task, Points = request.Points};
+        var newChore = new Chore() { Task = request.Task, Points = request.Points };
         _context.Chores.Add(newChore);
         await _context.SaveChangesAsync();
         return Ok(_context.Chores);
-    } 
-    
+    }
+
     [HttpPost("rewards")]
     public async Task<IActionResult> AddRewards(RewardRequest request)
     {
-        var newReward = new Reward() { Task = request.Task, Points = request.Points};
+        var newReward = new Reward() { Task = request.Task, Points = request.Points };
         _context.Rewards.Add(newReward);
         await _context.SaveChangesAsync();
         return Ok(_context.Chores);
     }
-    
-    // [HttpPut("id")]
-    // public async Task<IActionResult> UpdateComplete(RewardRequest rRequest)
-    // {
-    //     var chore = new Chore() { Chore.CompletionStatus = }
-    //     _context.Rewards.Update(Chore);
-    //     await _context.SaveChangesAsync();
-    //     return Ok(_context.Chores);
-    // }
+
 
     [HttpDelete("chores/{id}")]
     public async Task<IActionResult> Delete(int id)
     {
-        var chore = new Chore() {Id = id};
+        var chore = new Chore() { Id = id };
         var oneChore = _context.Chores.Find(id);
         if (oneChore == null) return NotFound();
         _context.Chores.Remove(oneChore);
+        await _context.SaveChangesAsync();
+        return Ok(oneChore);
+    }
+    
+    [HttpPut("chores/{id}")]
+    public async Task<IActionResult> Update(int id, bool completionStatus)
+    {
+        var oneChore = _context.Chores.Find(id);
+        if (oneChore == null) return NotFound();
+        if(oneChore.CompletionStatus == false) { oneChore.CompletionStatus = true; }
+        if(oneChore.CompletionStatus == true) { oneChore.CompletionStatus = false; }
+        var chore = new Chore() { Id = id, CompletionStatus = completionStatus};
+        _context.Chores.Update(oneChore);
         await _context.SaveChangesAsync();
         return Ok(oneChore);
     }
--- Hackday/chorereact/src/App.js
+++ Hackday/chorereact/src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [chore, setChore] = useState([]);
   const [reward, setReward] = useState([]);
   const [inputText, setInputText] = useState('');
+  const [pointTracker, setPointTracker] = useState(0);
   const [inputPoints, setInputPoints] = useState('');
 
   useEffect(() => {
     getChore(chore);
+    // getReward(reward);
   },
     [setChore]);
 
@@ -17,11 +19,10 @@ function App() {
     const deserializedJSON = await response.json();
     setChore(deserializedJSON);
   }
-
-  // const getRewards = async () => {
+  // const getReward = async () => {
   //   const response = await fetch("https://localhost:7147/ChoreGenerator/rewards");
   //   const deserializedJSON = await response.json();
-  //   setChore(deserializedJSON);
+  //   setReward(deserializedJSON);
   // }
 
   const deleteHandler = async (id) => {
@@ -33,21 +34,49 @@ function App() {
     };
     await fetch("https://localhost:7147/ChoreGenerator/chores/" + id, requestOptions);
     getChore();
+    alert('Chore removed from list');
   }
-  
-  const completeHandler = async (id, completionstatus) => {
+
+  let currentPoints = pointTracker;
+  const pointHandler = async (id, points) => {
     console.log(id);
-    console.log(completionstatus);
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    console.log(points);
+    setPointTracker(currentPoints += points);
     const requestOptions = {
-      method: 'PUT',
+      method: 'DELETE',
       mode: 'cors',
       redirect: 'follow'
     };
     await fetch("https://localhost:7147/ChoreGenerator/chores/" + id, requestOptions);
     getChore();
   }
+
+  const fakePointHandler = (points) => {
+    if(currentPoints < points){return alert('Insufficient funds')}
+    if(window.confirm(`Do you want to purchase this item for ${points} points?`)){
+      alert('Purchase Confirmed');
+      setPointTracker(currentPoints -= points);
+    } else {
+      alert('Purchase Canceled');
+    }
+  }
+
+  // const completeHandler = async (id, completionstatus) => {
+  //   console.log(id);
+  //   console.log(completionstatus);
+  //   var myHeaders = new Headers();
+  //   myHeaders.append("Content-Type", "application/json");
+  //   const body = JSON.stringify({
+  //     CompletionStatus: completionstatus
+  //   })
+  //   const requestOptions = {
+  //     method: 'PUT',
+  //     mode: 'cors',
+  //     redirect: 'follow'
+  //   };
+  //   await fetch("https://localhost:7147/ChoreGenerator/chores/" + id, requestOptions);
+  //   getChore();
+  // }
 
   const sendChore = async (e) => {
     e.preventDefault();
@@ -75,46 +104,85 @@ function App() {
     setInputPoints('');
   }
 
+  // const sendReward = async (e) => {
+  //   e.preventDefault();
+  //   if (!Number(e.target["points"].value)) {
+  //     e.target["points"].value = 0
+  //   }
+  //   var myHeaders = new Headers();
+  //   myHeaders.append("Content-Type", "application/json");
+  //   const body = JSON.stringify({
+  //     Task: e.target["reward"].value,
+  //     Points: e.target["points"].value
+  //   })
+  //   console.log(body)
+  //   const requestOptions = {
+  //     method: 'POST',
+  //     mode: 'cors',
+  //     headers: myHeaders,
+  //     body: body,
+  //     redirect: 'follow'
+  //   };
+  //   const response = await fetch("https://localhost:7147/ChoreGenerator/rewards", requestOptions)
+  //   const deserializedJSON = await response.json();
+  //   setChore(deserializedJSON);
+  //   setInputText('');
+  //   setInputPoints('');
+  // }
+
   return (
-      <div className="App">
-        <div className='title'>Chore Organizer</div>
-        <form className='chore-form' onSubmit={sendChore}>
-          <div>Input a Chore:</div>
-          <div className='input-field'>
-          <input onChange={(e) => {setInputText(e.target.value)}} value={inputText} className='input-input' placeholder='Chore...' type="text" name='chore' autoComplete='off' required />
-          <input onChange={(e) => {setInputPoints(e.target.value)}} value={inputPoints} className='input-input' placeholder='Points...' type="number" name='points' autoComplete='off' required />
-          </div>
-          <button className='submit-button'>Submit</button>
-        </form>
-        <div className='chore-title'>Chore List:</div>
-        <div className='chore-list'>
-          {chore.map((item) => {
-            return (
-              <div className='chore-item'> {item.task}
-              <div>
-              +{item.points}
-              </div>
-                <div className='buttons'>
-                  <button className='complete-button' onClick={() => completeHandler(item.id, item.completionstatus)}>✓</button>
-                  <button className='delete-button' onClick={() => deleteHandler(item.id)}>x</button>
-                </div>
-              </div>
-            )
-          })}
+    <div className="App">
+      <div className='title'>Chore Organizer</div>
+      <form className='chore-form' onSubmit={sendChore}>
+        <div>Input a Chore:</div>
+        <div className='input-field'>
+          <input onChange={(e) => { setInputText(e.target.value) }} value={inputText} className='input-input' placeholder='Chore...' type="text" name='chore' autoComplete='off' required />
+          <input onChange={(e) => { setInputPoints(e.target.value) }} value={inputPoints} className='input-input' placeholder='Points...' type="number" name='points' autoComplete='off' required />
         </div>
-        {/* <div>Rewards List:</div> */}
-        {/* {reward.map((item) => {
-        return (
-          <div>Task: {item.task} || Points: {item.points} <button onClick={deleteHandler}> Delete </button> </div>
+        <button className='submit-button'>Submit</button>
+      </form>
+      <div className='current-points'> Current Points: {pointTracker} </div>
+      <div className='chore-title'>Chore List:</div>
+      <div className='chore-list'>
+        {chore.map((item) => {
+          return (
+            <div className='chore-item'> {item.task}
+              <div>
+                +{item.points}
+              </div>
+              <div className='buttons'>
+                <button className='complete-button' onClick={() => pointHandler(item.id, item.points)}>✓</button>
+                <button className='delete-button' onClick={() => deleteHandler(item.id)}>x</button>
+              </div>
+            </div>
           )
-        })} */}
-        {/* <form onSubmit={sendChore}>
-      <div>Input a Reward:</div>
-      <input value={inputText} placeholder='Write a Chore...' type="text" name='chore' autoComplete='off' required/>
-      <input value={inputText} placeholder='Points for chore:' type="number" name='points' autoComplete='off' required/>
-      <button>Submit</button>
-    </form> */}
+        })}
       </div>
+      <div className='chore-title'>Reward List:</div>
+      <div className='chore-list'>
+            <div className='chore-item'> Sweets <div>
+                Cost: 300
+              </div>
+              <div className='buttons'>
+                <button className='complete-button' onClick={() => fakePointHandler(300)}>Buy</button>
+              </div>
+            </div>
+            <div className='chore-item'> Watch a movie <div>
+                Cost: 400
+              </div>
+              <div className='buttons'>
+                <button className='complete-button' onClick={() => fakePointHandler(400)}>Buy</button>
+              </div>
+            </div>
+            <div className='chore-item'> Order Uber Eats <div>
+                Cost: 500
+              </div>
+              <div className='buttons'>
+                <button className='complete-button' onClick={() => fakePointHandler(500)}>Buy</button>
+              </div>
+            </div>
+      </div>
+    </div>
   );
 }
 
